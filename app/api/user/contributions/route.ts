8e07faf6c20565f2cc7f9b88f24769478969cafe
RahulSingh044 <rahulsingh.dev.36@gmail.com>
2026-04-03 08:45:22 +0530

    fix: build errors for vercel

--- app/api/user/contributions/route.ts
+++ app/api/user/contributions/route.ts
@@ -1,38 +1,34 @@
 import prisma from "@/lib/prisma";
-import { NextRequest, NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async (req: Request, res: Response) => {
+export const GET = async (req: NextRequest) => {
+  try {
+    const { searchParams } = new URL(req.url);
+    const userId = searchParams.get("userId");
 
-    try {
-
-        const { searchParams } = new URL(req.url);
-        const userId = searchParams.get("userId");
-
-        if (!userId) {
-            return NextResponse.json(
-                { success: false, message: "Missing required fields" },
-                { status: 400 }
-            );
-        }
-
-        const contributions = await prisma.issue.findMany({
-            where: {
-                userId: userId,
-            },
-        });
-
-        return NextResponse.json(
-            { success: true, data: contributions },
-            { status: 200 }
-        );
-
-    } catch (error) {
-        console.error("Unable to fetch Contributions", error);
-
-        return NextResponse.json(
-            { success: false, message: "Unable to fetch Contributions" },
-            { status: 500 }
-        )
+    if (!userId) {
+      return NextResponse.json(
+        { success: false, message: "Missing required fields" },
+        { status: 400 },
+      );
     }
 
-}
+    const contributions = await prisma.issue.findMany({
+      where: {
+        userId: userId,
+      },
+    });
+
+    return NextResponse.json(
+      { success: true, data: contributions },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error("Unable to fetch Contributions", error);
+
+    return NextResponse.json(
+      { success: false, message: "Unable to fetch Contributions" },
+      { status: 500 },
+    );
+  }
+};
